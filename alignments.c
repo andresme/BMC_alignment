33cac06722e7be24af1a2c8f42db2e8f3de2cec0
Melisa Carranza <melisacz@gmail.com>
2015-06-09 21:32:39 -0600

    smith waterman's in da HAUS

--- alignments.c
+++ alignments.c
@@ -77,7 +77,7 @@ void runSmithWaterman(char *v_string, char *w_string, enum ALIGNMENT_MODE mode, 
       initMatrix(free_left_free_right, free_left_free_right);
   }
 
-  void *(*__start_routine)(void *) = p_SmithWaterman;
+  void *(*__start_routine)(void *) = mode == gap_blocks ? p_SmithWatermanBlocks : p_SmithWaterman;
 
   runThreads(__start_routine, threads, mode);
 
@@ -168,8 +168,8 @@ void *p_SmithWatermanBlocks(void *ptr_to_tdata) {
     int i, j, wave, tStart, tEnd;
     array_max_t arraymax;
 
-    tStart = ((td->jmax / td->numThreads) * td->thread_id) + 1;
-    tEnd = tStart + (td->jmax / td->numThreads);
+    tStart = (ceil(((float)td->jmax / (float)td->numThreads)) * td->thread_id)+1;
+    tEnd = (ceil(((float)td->jmax / (float)td->numThreads)) * (td->thread_id+1));
 
     pthread_mutex_lock(&mutexStart);
     startedThreads--;
@@ -183,10 +183,11 @@ void *p_SmithWatermanBlocks(void *ptr_to_tdata) {
     for (wave = 1; wave <= td->imax + td->numThreads - 1; wave++) {
         i = wave - td->thread_id;
         if (i >= 1 && i <= td->imax) {
-            for (j = tStart; j < tEnd; j++) {
-                tempH[0] = H[i - 1][j - 1] + similarity_score(seq_w[i - 1], seq_v[j - 1]);
-                tempH[1] = B[i - 1][j - 1] + similarity_score(seq_w[i - 1], seq_v[j - 1]);
-                tempH[2] = C[i - 1][j - 1] + similarity_score(seq_w[i - 1], seq_v[j - 1]);
+            for (j = tStart; j <= tEnd; j++) {
+
+                tempH[0] = H[i - 1][j - 1] != INT_MIN ? H[i - 1][j - 1] + similarity_score(seq_w[i - 1], seq_v[j - 1]) : INT_MIN;
+                tempH[1] = B[i - 1][j - 1] != INT_MIN ? B[i - 1][j - 1] + similarity_score(seq_w[i - 1], seq_v[j - 1]) : INT_MIN;
+                tempH[2] = C[i - 1][j - 1] != INT_MIN ? C[i - 1][j - 1] + similarity_score(seq_w[i - 1], seq_v[j - 1]) : INT_MIN;
                 tempH[3] = 0;
                 arraymax = find_array_max(tempH, 4);
                 H[i][j] = arraymax.max;
@@ -205,9 +206,9 @@ void *p_SmithWatermanBlocks(void *ptr_to_tdata) {
                         break;
                 }
 
-                tempB[0] = H[i - 1][j] + score_table.continue_block_cost + score_table.new_block_cost;
-                tempB[1] = B[i - 1][j] + score_table.continue_block_cost;
-                tempB[2] = C[i - 1][j]  + score_table.continue_block_cost + score_table.new_block_cost;
+                tempB[0] = H[i - 1][j] != INT_MIN ? H[i - 1][j] + score_table.continue_block_cost + score_table.new_block_cost : INT_MIN;
+                tempB[1] = B[i - 1][j] != INT_MIN ? B[i - 1][j] + score_table.continue_block_cost : INT_MIN;
+                tempB[2] = C[i - 1][j] != INT_MIN ? C[i - 1][j]  + score_table.continue_block_cost + score_table.new_block_cost : INT_MIN;
                 tempB[3] = 0;
                 arraymax = find_array_max(tempB, 4);
                 B[i][j] = arraymax.max;
@@ -226,9 +227,9 @@ void *p_SmithWatermanBlocks(void *ptr_to_tdata) {
                         break;
                 }
 
-                tempC[0] = H[i][j - 1] + score_table.continue_block_cost + score_table.new_block_cost;
-                tempC[1] = B[i][j - 1] + score_table.continue_block_cost + score_table.new_block_cost;
-                tempC[2] = C[i][j - 1] + score_table.continue_block_cost;
+                tempC[0] = H[i][j - 1] != INT_MIN ? H[i][j - 1] + score_table.continue_block_cost + score_table.new_block_cost : INT_MIN;
+                tempC[1] = B[i][j - 1] != INT_MIN ? B[i][j - 1] + score_table.continue_block_cost + score_table.new_block_cost : INT_MIN;
+                tempC[2] = C[i][j - 1] != INT_MIN ? C[i][j - 1] + score_table.continue_block_cost : INT_MIN;
                 tempC[3] = 0;
                 arraymax = find_array_max(tempC, 4);
                 C[i][j] = arraymax.max;
