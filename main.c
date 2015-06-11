eaa89e5497d1ce2456a72afcdaa1833ff9c1eff6
IreneGamboa <ireniuxgp@gmail.com>
2015-06-10 20:55:45 -0600

    Interfaz

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include "globals.h"
 #include "utils.h"
 #include "alignments.h"
+#include <gtk/gtk.h> 
+
+GtkWidget *entrada, *nw, *sw;
+GtkEntry  *h1, *h2, *cantThreads ,*match, *miss, *gap, *valueK, *ampB, *valueg, *valueh;
+GtkWidget *matriz, *results;
+GtkTextBuffer *buffer, *bufferResults;
+GtkBuilder *builder;
+
+
 
 int main(int argc, const char* argv[]) {
+	
+	GError *error = NULL;
+	gtk_init(&argc, &argv);
+	
+    builder = gtk_builder_new();
+    
+    if(!gtk_builder_add_from_file(builder, "entrada.glade", &error)) {
+		g_warning("%s", error->message);
+		g_free(error);
+		return(1);
+	}
+   	getGtkWidgets(builder);
+	gtk_builder_connect_signals(builder, NULL);
+	g_object_unref(G_OBJECT(builder));
+	gtk_widget_show(entrada);
+	gtk_main();
+		
+	return 0;
 
     score_table.gap = -2;
     score_table.match = 1;
@@ -18,3 +45,16 @@ int main(int argc, const char* argv[]) {
 
     pthread_exit(NULL);
 }
+
+
+void getGtkWidgets(GtkBuilder *builder){
+		entrada = GTK_WIDGET(gtk_builder_get_object(builder, "entrada"));
+		cantThreads = (GtkEntry *) GTK_WIDGET(gtk_builder_get_object(builder, "threads"));
+		h1 = (GtkEntry *) GTK_WIDGET(gtk_builder_get_object(builder, "h1"));
+		h2 = (GtkEntry *) GTK_WIDGET(gtk_builder_get_object(builder, "h2"));
+		match = (GtkEntry *) GTK_WIDGET(gtk_builder_get_object(builder, "match"));
+		miss = (GtkEntry *) GTK_WIDGET(gtk_builder_get_object(builder, "miss"));
+		gap = (GtkEntry *) GTK_WIDGET(gtk_builder_get_object(builder, "gap"));
+
+		
+	}
